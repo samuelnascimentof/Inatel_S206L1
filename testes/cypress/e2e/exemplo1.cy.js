6e6f54ebf253e9a6274178c0163e32b3d0054360
Samuel Nascimento Filho <samuel.nascimento@datacoress.com>
2023-11-12 18:27:22 -0300

    Add códigos aula 4

--- testes/cypress/e2e/exemplo1.cy.js
+++ testes/cypress/e2e/exemplo1.cy.js
@@ -2,7 +2,7 @@
 
 describe('Criando cenário de teste para o site globalsqa', () => {
 
-  it.skip('Caso de teste: Registrando um usuário no site com sucesso', () => {
+  it('Caso de teste: Registrando um usuário no site com sucesso', () => {
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
     cy.get('.btn-link').click()
     cy.get('#firstName').type('inatel')
@@ -13,7 +13,7 @@ describe('Criando cenário de teste para o site globalsqa', () => {
     cy.get('.ng-binding').should('contain.text', 'Registration successful')
   })
 
-  it.skip('Caso de teste: Registrando um usuário no site com falha (faltando senha)', () => {
+  it('Caso de teste: Registrando um usuário no site com falha (faltando senha)', () => {
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register')
     cy.get('#firstName').type('inatel')
     cy.get('#Text1').type('inatel')
@@ -31,6 +31,15 @@ describe('Criando cenário de teste para o site globalsqa', () => {
     cy.get('#password').type(info[1])
     cy.get('.btn-primary').click()
     cy.get('h1.ng-binding').should('contain.text', info[0])
+  })
+
+  it('Caso de teste: Deletando um usuário com sucesso', () => {
+    const info = criarUsuario()
+    cy.login(info[0], info[1])
+    cy.get('.ng-binding > a').click()
+    cy.get('.btn').click()
+    cy.login(info[0], info[1])
+    cy.get('.ng-binding').should('have.text', 'Username or password is incorrect')
   })
 
   it.skip("Caso de teste: Realizando login com falha (senha incorreta)", () => {
@@ -67,8 +76,8 @@ function criarUsuario(){
   cy.get('#firstName').type(user)
   cy.get('#Text1').type(user)
   cy.get('#username').type(user)
-  cy.get('#password').type(user)
-  cy.get('.btn-primary').click(senha)
+  cy.get('#password').type(senha)
+  cy.get('.btn-primary').click()
   cy.get('.ng-binding').should('contain.text', 'Registration successful')
 
   return userInfo
